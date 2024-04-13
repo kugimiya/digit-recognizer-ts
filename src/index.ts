e3a63f80e082a104e3f85f72f8afd07130a5cee9
kugimiya <bp.001@yandex.ru>
2024-04-13 14:56:15 +0400

    chore: tune

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ import { Trainer } from "./lib/Trainer";
 import { Network } from "./lib/Network";
 import path from "node:path";
 
+// Инит тредов для параллельной работы
 const worker_farm = require('worker-farm');
-
 const farm = worker_farm({ maxConcurrentCallsPerWorker: 1, autoStart: true }, path.resolve(__dirname, 'lib/Worker'));
 
+// максимум "эпох"
 const MAX_EPOCHS = 2;
+// кол-во микро-мутаций при переходе в датасете
 const MAX_MUTATIONS_VARIANTS = 16;
 
+// Инит обучалки
 console.log('INFO: reading dataset');
 const trainer = new Trainer(path.resolve(__dirname, '../mnist_digits_dataset.csv'), farm);
 
+// Инит пустой нейронки
 console.log('INFO: making network');
 const network = new Network();
 network.make([784, 784 / 2, 784 / 4, 784 / 8, 784 / 16, 10]);
 
+// Запуск обучалки, в конце будет получена обученная нейронка и записана в файл
 console.log('INFO: training network');
 trainer.train_network(MAX_EPOCHS, MAX_MUTATIONS_VARIANTS, network).then(coolest_network => {
   try {
