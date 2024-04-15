ec7d7186efd54eb5f3ed67a52fdfb29d6ec466fb
kugimiya <bp.001@yandex.ru>
2024-04-15 19:14:08 +0400

    feature: SGD impl

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import path from "node:path";
 import { Network } from "./lib/Network";
 
@@ -6,7 +6,27 @@ import { Network } from "./lib/Network";
 // const worker_farm = require('worker-farm');
 // const farm = worker_farm({ maxConcurrentCallsPerWorker: 1, autoStart: false }, path.resolve(__dirname, 'lib/Worker'));
 
-const dataset = readFileSync(path.resolve(__dirname, '..', 'mnist_digits_dataset.csv'))
+const getShuffledArr = <T>(arr: T[]): T[] => {
+  const newArr = arr.slice(0);
+
+  for (let i = newArr.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
+  }
+
+  return newArr;
+};
+
+const test_datasets_file_names = [
+  'test_mnist_aa', 'test_mnist_ab', 'test_mnist_ac', 'test_mnist_ad', 'test_mnist_ae', 'test_mnist_af', 'test_mnist_ag'
+];
+
+const train_datasets_file_names = [
+  'train_mnist_a0', 'train_mnist_aa', 'train_mnist_ab', 'train_mnist_ac', 'train_mnist_ad', 'train_mnist_ae', 'train_mnist_af', 'train_mnist_ag', 'train_mnist_ah', 'train_mnist_ai', 'train_mnist_aj', 'train_mnist_ak', 'train_mnist_al', 'train_mnist_am', 'train_mnist_an', 'train_mnist_ao', 'train_mnist_ap', 'train_mnist_aq', 'train_mnist_ar', 'train_mnist_as', 'train_mnist_at', 'train_mnist_au', 'train_mnist_av', 'train_mnist_aw', 'train_mnist_ax', 'train_mnist_ay', 'train_mnist_az', 'train_mnist_ba', 'train_mnist_bb', 'train_mnist_bc', 'train_mnist_bd', 'train_mnist_be', 'train_mnist_bf', 'train_mnist_bg', 'train_mnist_bh', 'train_mnist_bi', 'train_mnist_bj', 'train_mnist_bk', 'train_mnist_bl', 'train_mnist_bm', 'train_mnist_bn'
+];
+
+
+const load_datasets = (filenames: string[]) => filenames.map((file_name) => readFileSync(path.resolve(__dirname, '..', 'datasets', file_name))
   .toString()
   .split('\n')
   .slice(2, -2)
@@ -26,29 +46,77 @@ const dataset = readFileSync(path.resolve(__dirname, '..', 'mnist_digits_dataset
       digit === 8 ? 1 : 0,
       digit === 9 ? 1 : 0,
     ]
-  }));
+  }))
+).reduce((acc, cur) => [...acc, ...cur],[]);
 
-function run_dataset(origin_network: Network, dataset: { digit: number, input: number[], output: number[] }[]) {
+const train_dataset = load_datasets(train_datasets_file_names);
+const test_dataset = load_datasets(test_datasets_file_names);
+
+function run_dataset(origin_network: Network, dataset: { digit: number, input: number[], output: number[] }[], learn_rate: number) {
   let errors: number[] = [];
-  let networks: Network[] = [];
   let common_error = 0;
+  let network = origin_network.clone();
 
-  dataset.forEach(({ input, output }) => {
-    const network = origin_network.clone();
-    network.run(input);
+  let false_predictions = 0;
+  let true_predictions = 0;
+
+  dataset.forEach(({ input, output, digit }) => {
+    network.feed_forward(input);
     const error = network.get_error(output);
     common_error = common_error + error;
-    networks.push(network);
     errors.push(error);
+
+    const result = network.get_result_softed().map((v) => v > 0.5 ? 1 as number : 0 as number);
+
+    if (result[digit] === 1 && result.reduce((a, c) => a + c, 0) === 1) {
+      true_predictions += 1;
+    } else {
+      false_predictions += 1;
+    }
+
+    network.back_propagation(learn_rate, output);
   });
 
   common_error = common_error / errors.length;
 
-  return { common_error, errors, networks };
+  return { common_error, errors, network, false_predictions, true_predictions };
 }
 
-let network = new Network([784, 16, 16, 10]);
+let network = new Network([784, 784 / 2, 784 / 4, 784 / 8, 784 / 16, 10]);
 network.randomize();
-const result = run_dataset(network, dataset);
 
-console.log(result.common_error);
+let epoch_count = 1024;
+let batch_size = 100;
+let learn_rate = 0.001;
+let nice_ratio = 0.95;
+let prev_ratio = 0;
+
+while (prev_ratio < nice_ratio) {
+  console.log('Start training');
+
+  for (let i = 0; i < epoch_count; i++) {
+    console.log(`Run iteration: ${i + 1} of ${epoch_count}, with batch_size=${batch_size} ...`);
+    const result = run_dataset(network, getShuffledArr(train_dataset).slice(0, batch_size), learn_rate);
+    network = result.network;
+    console.log(`... completed, with avg_error=${result.common_error}, true_pred=${result.true_predictions} / ${batch_size}\n`);
+  }
+
+  writeFileSync(path.resolve(__dirname, '..', 'weights.json'), JSON.stringify(network.clone().clear()));
+
+  console.log('Run test dataset');
+
+  let true_predictions = 0;
+  test_dataset.forEach(({ input, digit }) => {
+    network.feed_forward(input);
+    const result = network.get_result_softed().map((v) => v > 0.5 ? 1 as number : 0 as number);
+    if (result[digit] === 1 && result.reduce((a, c) => a + c, 0) === 1) {
+      true_predictions += 1;
+    }
+  });
+
+  prev_ratio = true_predictions / test_dataset.length;
+  console.log(`Test done! True predictions: ${true_predictions} of ${test_dataset.length} ratio=${prev_ratio}`);
+  if (prev_ratio < nice_ratio) {
+    console.log(`Ratio (${prev_ratio} is not nice (${nice_ratio}), start training again...`);
+  }
+}
--- src/lib/Network.ts
+++ src/lib/Network.ts
@@ -1,76 +1,144 @@
 export type Layer = {
-  neuron_biases: number[];
-  out_weights: number[];
-}
-
-export type LayerResult = {
-  input_sums: number[];
+  size: number;
+  biases: number[];
+  weights: number[][];
   activations: number[];
 }
 
 export const sigmoid = (x: number) => {
-  return Math.exp(x) / (Math.exp(x) + 1)
+  return 1 / (1 + Math.exp(-x));
+};
+
+export const sigmoid_prime = (x: number) => {
+  return x * (1 - x);
 };
 
 export class Network {
   layers: Layer[] = [];
-  layers_results: LayerResult[] = [];
 
   constructor(neurons_counts: number[]) {
     for (let i = 0; i < neurons_counts.length; i++) {
-      const weights_count = i === neurons_counts.length - 1
-        ? 0
-        : neurons_counts[i] * neurons_counts[i + 1];
+      let nextSize = 0;
 
-      this.layers.push({
-        neuron_biases: new Array(neurons_counts[i]).fill(0),
-        out_weights: new Array(weights_count).fill(0)
-      });
+      if (i < neurons_counts.length - 1) {
+        nextSize = neurons_counts[i + 1];
+      }
 
-      this.layers_results.push({
-        input_sums: new Array(neurons_counts[i]).fill(0),
+      this.layers[i] = {
+        size: neurons_counts[i],
+        biases: new Array(neurons_counts[i]).fill(0),
+        weights: new Array(neurons_counts[i]).fill(0).map(() => new Array(nextSize).fill(0)),
         activations: new Array(neurons_counts[i]).fill(0)
-      });
+      };
     }
   }
 
   randomize() {
-    this.layers.forEach((layer) => {
-      layer.out_weights = layer.out_weights.map(() => Math.random() - 0.5);
-      layer.neuron_biases = layer.neuron_biases.map(() => Math.random() - 0.5);
-    });
-  }
+    for (let i = 0; i < this.layers.length; i++) {
+      let nextSize = 0;
 
-  run(input: number[]) {
-    // set input as activations of first layer
-    input.forEach((value, index) => {
-      this.layers_results[0].activations[index] = value;
-    });
+      if (i < this.layers.length - 1) {
+        nextSize = this.layers[i + 1].size;
+      }
 
-    // feed layers :^)
-    for (let layer_index = 1; layer_index < this.layers.length; layer_index++) {
-      let previous_activations = this.layers_results[layer_index - 1].activations;
+      for (let j = 0; j < this.layers[i].size; j++) {
+        this.layers[i].biases[j] = Math.random() * 2.0 - 1.0;
 
-      for (let neuron_index = 0; neuron_index < this.layers[layer_index].neuron_biases.length; neuron_index++) {
-        const weight_offset = neuron_index * this.layers[layer_index - 1].neuron_biases.length;
-        const bias = this.layers[layer_index].neuron_biases[neuron_index];
-        const input_sum = previous_activations
-          .map((value, index) => value * this.layers[layer_index - 1].out_weights[weight_offset + index])
-          .reduce((acc, val) => acc + val, 0);
-
-        const pre_activation = input_sum + bias;
-        this.layers_results[layer_index].input_sums[neuron_index] = pre_activation;
-        this.layers_results[layer_index].activations[neuron_index] = sigmoid(pre_activation);
+        for (let k = 0; k < nextSize; k++) {
+          this.layers[i].weights[j][k] = Math.random() * 2.0 - 1.0;
+        }
       }
     }
   }
 
+  get last_layer(): Layer {
+    return this.layers.at(-1) as Layer;
+  }
+
+  feed_forward(input: number[]) {
+    // set input as activations of first layer
+    input.forEach((value, index) => {
+      this.layers[0].activations[index] = value;
+    });
+
+    // feed layers :^)
+    for (let i = 1; i < this.layers.length; i++)  {
+      let prev_layer = this.layers[i - 1];
+      let curr_layer = this.layers[i];
+
+      for (let j = 0; j < curr_layer.size; j++) {
+        curr_layer.activations[j] = 0;
+        for (let k = 0; k < prev_layer.size; k++) {
+          curr_layer.activations[j] += prev_layer.activations[k] * prev_layer.weights[k][j];
+        }
+        curr_layer.activations[j] += curr_layer.biases[j];
+        curr_layer.activations[j] = sigmoid(curr_layer.activations[j]);
+      }
+    }
+  }
+
+  back_propagation(learn_rate: number, output: number[]): Network {
+    // calc errors
+    let errors = new Array(this.last_layer.activations.length).fill(0);
+    for (let i = 0; i < this.last_layer.activations.length; i++) {
+      errors[i] = output[i] - this.last_layer.activations[i];
+    }
+
+    // iterate over layers and calc/apply gradients, errors, weights, biases; skip last layer from iteration
+    for (let layer_index = this.layers.length - 2; layer_index >= 0; layer_index--) {
+      const current_layer = this.layers[layer_index];
+      const prev_layer = this.layers[layer_index + 1];
+
+      // calc gradients
+      const gradients = new Array(prev_layer.size).fill(0);
+      for (let i = 0; i < prev_layer.size; i++) {
+        gradients[i] = errors[i] * sigmoid_prime(prev_layer.activations[i]);
+        gradients[i] *= learn_rate;
+      }
+
+      // calc deltas
+      const deltas: number[][] = new Array(prev_layer.size).fill(0).map(() => new Array(current_layer.size).fill(0));
+      for (let i = 0; i < prev_layer.size; i++) {
+        for (let j = 0; j < current_layer.size; j++) {
+          deltas[i][j] = gradients[i] * current_layer.activations[j];
+        }
+      }
+
+      // calc next errors
+      const next_errors = new Array(current_layer.size).fill(0);
+      for (let i = 0; i < current_layer.size; i++) {
+        for (let j = 0; j < prev_layer.size; j++) {
+          next_errors[i] += current_layer.weights[i][j] * errors[j];
+        }
+      }
+
+      // swap errors
+      errors = next_errors.slice(0);
+
+      // calc/apply new weights
+      let next_weights = new Array(current_layer.weights.length).fill(0).map(() => new Array(current_layer.weights[0].length).fill(0));
+      for (let i = 0; i < prev_layer.size; i++) {
+        for (let j = 0; j < current_layer.size; j++) {
+          next_weights[j][i] = current_layer.weights[j][i] + deltas[i][j];
+        }
+      }
+      current_layer.weights = next_weights.slice(0).map(_ => _.slice(0));
+
+      // apply gradients to biases
+      for (let i = 0; i < prev_layer.size; i++) {
+        prev_layer.biases[i] += gradients[i];
+      }
+    }
+
+    return this;
+  }
+
   get_result_softed() {
-    return (this.layers_results.at(-1)?.activations || []).map(v => Math.floor(v * 10) / 10);
+    return (this.last_layer.activations || []).map(v => Math.floor(v * 10) / 10);
   }
 
   get_error(output: number[]) {
-    return (this.layers_results.at(-1)?.activations || [] as number[])
+    return (this.last_layer.activations || [] as number[])
       .map((value, index) => {
         return (value + output[index]) * (value + output[index]);
       })
@@ -78,9 +146,23 @@ export class Network {
   }
 
   clone(): Network {
-    const next_net = new Network(this.layers.map(_ => _.neuron_biases.length));
-    next_net.layers = this.layers.map((l) => ({ neuron_biases: l.neuron_biases.slice(0), out_weights: l.out_weights.slice(0) }));
-    next_net.layers_results = this.layers_results.map((r) => ({ input_sums: r.input_sums.slice(0), activations: r.activations.slice(0) }));
+    const next_net = new Network(this.layers.map(_ => _.size));
+    next_net.layers = this.layers.map((l) => ({
+      biases: l.biases.slice(0),
+      weights: l.weights.slice(0).map(_ => _.slice(0)),
+      activations: l.activations.slice(0),
+      size: l.size,
+    }));
     return next_net;
+  }
+
+  clear(): Network {
+    this.layers = this.layers.map((l) => ({
+      biases: l.biases.slice(0),
+      weights: l.weights.slice(0).map(_ => _.slice(0)),
+      activations: new Array(l.activations.length).fill(0),
+      size: l.size,
+    }));
+    return this;
   }
 }
